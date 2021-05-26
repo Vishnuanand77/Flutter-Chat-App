cced037d5fd1a7f9c98b17886c2cbdbcd785ce78
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-27 00:16:24 +0530

    Added chat message bubbles

--- lib/Widgets/Chat/messages.dart
+++ lib/Widgets/Chat/messages.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/Widgets/Chat/message_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -21,9 +22,7 @@ class Messages extends StatelessWidget {
         final chatDocs = chatSnapshot.data.documents;
         return ListView.builder(
           reverse: true,
-          itemBuilder: (ctx, index) => Text(
-            chatDocs[index]['text'],
-          ),
+          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
           itemCount: chatDocs.length,
         );
       },
