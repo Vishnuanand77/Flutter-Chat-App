21d926991be260244d782c6a144db3d7195deb1d
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-26 23:24:08 +0530

    Added Logout functionality

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Flutter Chat',
+          style: TextStyle(
+            fontFamily: 'billabong',
+            fontSize: 30,
+          ),
+        ),
+        actions: [
+          DropdownButton(
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+          )
+        ],
+      ),
       body: StreamBuilder(
         stream: Firestore.instance
             .collection('chats/TxKIxCdx6mtAFhm6etP0/messages')
@@ -28,9 +67,9 @@ class ChatScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Firestore.instance.collection('chats/TxKIxCdx6mtAFhm6etP0/messages').add({
-            'text': 'This was added by a FAB click!'
-          });
+          Firestore.instance
+              .collection('chats/TxKIxCdx6mtAFhm6etP0/messages')
+              .add({'text': 'This was added by a FAB click!'});
         },
       ),
     );
