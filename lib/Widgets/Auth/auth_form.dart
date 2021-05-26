ec337fcc1297eb5064dc784706aec7fa479129e0
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-26 21:10:15 +0530

    Added form validation

--- lib/Widgets/Auth/auth_form.dart
+++ lib/Widgets/Auth/auth_form.dart
@@ -6,6 +6,27 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate(); //Triggers validation in all form fields
+    FocusScope.of(context).unfocus(); //Closes the soft keyboard when submit is pressed.
+
+    if (isValid) {
+      _formKey.currentState.save(); //Triggers on saved function in all form fields
+
+      //User saved values to send auth request
+
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,23 +36,51 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(labelText: 'Email Address'),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
                   ),
                   TextFormField(
                     decoration: InputDecoration(labelText: 'Username'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 4) {
+                        return 'Please enter at least 4 characters';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userName = value;
+                    },
                   ),
                   TextFormField(
-                    decoration: InputDecoration(labelText: 'Username'),
+                    decoration: InputDecoration(labelText: 'Password'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
                     obscureText: true,
                   ),
                   SizedBox(height: 12),
                   RaisedButton(
-                    onPressed: () {},
+                    onPressed: _trySubmit,
                     child: Text('Login'),
                   ),
                   FlatButton(
