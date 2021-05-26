5f0ae6b30a8a78cef946556e7e16362ae785f1d6
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-26 22:28:12 +0530

    Made minor UI changes

--- lib/Widgets/Auth/auth_form.dart
+++ lib/Widgets/Auth/auth_form.dart
@@ -48,83 +48,99 @@ class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Card(
-        margin: EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    'Flutter Chat',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+      child: Container(
+        decoration: new BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.black45,
+              blurRadius: 40.0,
+              spreadRadius: 5.0,
+            ),
+          ],
+        ),
+        child: Card(
+          elevation: 40,
+          margin: EdgeInsets.all(20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Flutter Chat',
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Billabong',
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 12),
-                  TextFormField(
-                    key: ValueKey('email'),
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(labelText: 'Email Address'),
-                    validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Please enter a valid email address.';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _userEmail = value;
-                    },
-                  ),
-                  if (!_isLogin)
+                    SizedBox(height: 12),
                     TextFormField(
-                      key: ValueKey('username'),
-                      decoration: InputDecoration(labelText: 'Username'),
+                      key: ValueKey('email'),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(labelText: 'Email Address'),
                       validator: (value) {
-                        if (value.isEmpty || value.length < 4) {
-                          return 'Please enter at least 4 characters';
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Please enter a valid email address.';
                         }
                         return null;
                       },
                       onSaved: (value) {
-                        _userName = value;
+                        _userEmail = value;
                       },
                     ),
-                  TextFormField(
-                    key: ValueKey('password'),
-                    decoration: InputDecoration(labelText: 'Password'),
-                    validator: (value) {
-                      if (value.isEmpty || value.length < 7) {
-                        return 'Password must be at least 7 characters long.';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _userPassword = value;
-                    },
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 12),
-                  RaisedButton(
-                    onPressed: _trySubmit,
-                    child: Text(_isLogin ? 'Login' : 'Sign Up'),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    },
-                    child: Text(_isLogin
-                        ? 'Create new Account'
-                        : 'I already have an account'),
-                    textColor: Theme.of(context).primaryColor,
-                  ),
-                ],
+                    if (!_isLogin)
+                      TextFormField(
+                        key: ValueKey('username'),
+                        decoration: InputDecoration(labelText: 'Username'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 4) {
+                            return 'Please enter at least 4 characters';
+                          }
+                          return null;
+                        },
+                        onSaved: (value) {
+                          _userName = value;
+                        },
+                      ),
+                    TextFormField(
+                      key: ValueKey('password'),
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 7) {
+                          return 'Password must be at least 7 characters long.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _userPassword = value;
+                      },
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 12),
+                    RaisedButton(
+                      onPressed: _trySubmit,
+                      child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      child: Text(_isLogin
+                          ? 'Create new Account'
+                          : 'I already have an account'),
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
