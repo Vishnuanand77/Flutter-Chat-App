bfa522f541d137fecdb9c2ba5b465b106b3fbaa0
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-26 21:54:54 +0530

    Added context information to submitFn

--- lib/Screens/auth_screen.dart
+++ lib/Screens/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 import '../Widgets/Auth/auth_form.dart';
 
@@ -8,10 +10,42 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  //Using firebase SDK to create a new user or sign an existing user in.
 
-  void _submitAuthForm(String email, String password, String username, bool isLogin) {
-    //Using firebase SDK to create a new user or sign an existing user in.
+  final _auth = FirebaseAuth.instance;
 
+  void _submitAuthForm(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (err) {
+      //PlatformException is to filter errors from firebase
+      var message = 'Oops! An error occured. Please check your credentials!';
+
+      if (err.message != null) {
+        message = err.message;
+      }
+
+      Scaffold.of(ctx).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(ctx).errorColor,
+      ));
+    } catch (err) {
+      print(err);
+    }
   }
 
   @override
