92b2103ecfbc2ecbe1054d70934a057eeb855cde
Vishnu Anand <vishnuanand2000@gmail.com>
2021-05-26 18:09:13 +0530

    Fetch text data from firebase functionality added

--- lib/Screens/chat_screen.dart
+++ lib/Screens/chat_screen.dart
@@ -10,20 +10,24 @@ class ChatScreen extends StatelessWidget {
             .collection('chats/TxKIxCdx6mtAFhm6etP0/messages')
             .snapshots(),
         builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
           return ListView.builder(
-            itemCount: streamSnapshot.data.documents.length,
+            itemCount: documents.length,
             itemBuilder: (ctx, index) => Container(
               padding: EdgeInsets.all(8),
-              child: Text('This works!'),
+              child: Text(documents[index]['text']),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-
-        },
+        onPressed: () {},
       ),
     );
   }
